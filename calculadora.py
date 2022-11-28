0a5638082fc0c9568e22a9667446ccc18bf17123
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-27 23:50:48 -0400

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+# Calculadora básica #
+
+print('CALCULADORA PYTHON\n')
+print('Escolha ma das opções a baixo para calcular:\n')
+print('1- Soma')
+print('2- Subtração')
+print('3- Multiplicação')
+print('4- Divisão\n\n')
+
+escolha = int(input('Digite uma das opções (1/2/3/4): '))
+
+if escolha == 1:
+    valor1 = int(input('\nDigite o primeiro número: '))
+    valor2 = int(input('Digite o segundo número: '))
+    soma = valor1 + valor2
+    print(f'O resultado da soma é {soma}')
