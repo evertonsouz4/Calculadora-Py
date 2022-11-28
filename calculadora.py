6dee990861ef81a50785bc82acdcb18afa9d99d1
Everton Souza <everton.souzaa@outlook.com.br>
2022-11-28 05:39:36 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -13,4 +13,16 @@ if escolha == 1:
     valor1 = int(input('\nDigite o primeiro número: '))
     valor2 = int(input('Digite o segundo número: '))
     soma = valor1 + valor2
-    print(f'O resultado da soma é {soma}')
+    print(f'O resultado de {valor1} + {valor2} é {soma}.')
+
+elif escolha == 2:
+    valor3 = int(input('\nDigite o primeiro número: '))
+    valor4 = int(input('Digite o segundo número: '))
+    subtr = valor3 - valor4
+    print(f'O resultado de {valor3} - {valor4} é {subtr}.')
+
+elif escolha == 3:
+    valor5 = int(input('\nDigite o primeiro número: '))
+    valor6 = int(input('Digite o segundo número: '))
+    multiply = valor5 * valor6
+    print(f'O resultado de {valor5} * {valor6} é {multiply}.')
